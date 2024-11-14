9364bef064de4814992cf2953afdcef1322c3ca5
Igor Vasconcellos <143509020+igor-vasconcellos@users.noreply.github.com>
2024-11-14 11:38:48 -0300

    Add files via upload

--- portifolio/js/script.js
+++ portifolio/js/script.js
@@ -4,9 +4,4 @@ const navbar = document.querySelector(".navbar");
 menuIcon.onclick = () => {
   navbar.classList.toggle("active");
 };
-// Selecionando o botão corretamente usando a classe 'download-btn'
-const button = document.querySelector(".download-btn");
 
-button.addEventListener("click", () => {
-  button.classList.add("active"); // Adiciona a classe 'active' ao botão quando ele é clicado
-});
